0a582f208bf4def21bce6c6efce952a4518efd73
Petr Kalis <petr.kalis@gmail.com>
2024-07-12 18:51:15 +0200

    Updates to run Photoshop in automatic tests

--- client/ayon_photoshop/api/lib.py
+++ client/ayon_photoshop/api/lib.py
@@ -2,6 +2,8 @@ import os
 import sys
 import contextlib
 import traceback
+import functools
+import pyblish
 
 from ayon_core.lib import env_value_to_bool, Logger, is_in_tests
 from ayon_core.addon import AddonsManager
@@ -34,7 +36,18 @@ def main(*subprocess_args):
     launcher = ProcessLauncher(subprocess_args)
     launcher.start()
 
-    if env_value_to_bool("HEADLESS_PUBLISH"):
+    if is_in_tests():
+        manager = AddonsManager()
+        photoshop_addon = manager["photoshop"]
+
+        launcher.execute_in_main_thread(
+            functools.partial(
+                photoshop_addon.publish_in_test,
+                log,
+                "ClosePS",
+            )
+        )
+    elif env_value_to_bool("HEADLESS_PUBLISH"):
         manager = AddonsManager()
         webpublisher_addon = manager["webpublisher"]
         launcher.execute_in_main_thread(
@@ -82,3 +95,44 @@ def maintained_visibility(layers=None):
         for layer in layers:
             stub().set_visible(layer.id, visibility[layer.id])
             pass
+
+
+def find_close_plugin(close_plugin_name, log):
+    if close_plugin_name:
+        plugins = pyblish.api.discover()
+        for plugin in plugins:
+            if plugin.__name__ == close_plugin_name:
+                return plugin
+
+    log.debug("Close plugin not found, app might not close.")
+
+
+def publish_in_test(log, close_plugin_name=None):
+    """Loops through all plugins, logs to console. Used for tests.
+    Args:
+        log (Logger)
+        close_plugin_name (Optional[str]): Name of plugin with responsibility
+            to close application.
+    """
+
+    # Error exit as soon as any error occurs.
+    error_format = "Failed {plugin.__name__}: {error} -- {error.traceback}"
+    close_plugin = find_close_plugin(close_plugin_name, log)
+
+    for result in pyblish.util.publish_iter():
+        for record in result["records"]:
+            # Why do we log again? pyblish logger is logging to stdout...
+            log.info("{}: {}".format(result["plugin"].label, record.msg))
+
+        if not result["error"]:
+            continue
+
+        # QUESTION We don't break on error?
+        error_message = error_format.format(**result)
+        log.error(error_message)
+        if close_plugin:  # close host app explicitly after error
+            context = pyblish.api.Context()
+            try:
+                close_plugin().process(context)
+            except Exception as exp:
+                print(exp)
