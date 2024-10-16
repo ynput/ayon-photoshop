4a28c196ee782a392b0718686068fd7d4ae452ba
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-16 10:58:39 +0200

    define app host name

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "photoshop"
 title = "Photoshop"
 version = "0.2.3+dev"
-
+app_host_name = "photoshop"
 client_dir = "ayon_photoshop"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
