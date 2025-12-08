e2d62d371cc75aac6ed3425b877329a855964d9b
Jakub Jezek <jakubjezek001@gmail.com>
2025-12-08 12:29:35 +0100

    reverting

--- client/ayon_photoshop/api/ws_stub.py
+++ client/ayon_photoshop/api/ws_stub.py
@@ -334,7 +334,7 @@ class PhotoshopServerStub:
 
     def merge_all_layersets(self, parent_set=None):
         """Merges layer sets into one layer.
-
+        
         Args:
             parent_set (str): id of layer set to merge layers sets it contains.
                 If None, all first level layer sets will be merged.
@@ -499,7 +499,7 @@ class PhotoshopServerStub:
         exclude_ids = {layer.id for layer in exclude_layers}
         if exclude_recursive:
             exclude_ids |= {ll.id for ll in self.get_layers_in_layers(exclude_layers)}
-
+        
         for layer in self.get_layers():
             if layer.id not in exclude_ids:
                 self.delete_layer(layer.id)
