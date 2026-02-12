81347c68f0c354b7d987ff68a53eb116fb35b74d
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-12 10:47:26 +0100

    convert settings and code for color coded instances

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,12 @@
+from typing import Any
+
 from ayon_server.addons import BaseServerAddon
 
-from .settings import PhotoshopSettings, DEFAULT_PHOTOSHOP_SETTING
+from .settings import (
+    convert_settings_overrides,
+    DEFAULT_PHOTOSHOP_SETTING,
+    PhotoshopSettings,
+)
 
 
 class Photoshop(BaseServerAddon):
@@ -9,3 +15,13 @@ class Photoshop(BaseServerAddon):
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
         return settings_model_cls(**DEFAULT_PHOTOSHOP_SETTING)
+
+    async def convert_settings_overrides(
+        self,
+        source_version: str,
+        overrides: dict[str, Any],
+    ) -> dict[str, Any]:
+        convert_settings_overrides(source_version, overrides)
+        return await super().convert_settings_overrides(
+            source_version, overrides
+        )
