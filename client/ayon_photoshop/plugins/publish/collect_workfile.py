3f164a3b129ca2149b91545bf9be8f12cac66c29
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-12 15:06:17 +0100

    safe extension split

--- client/ayon_photoshop/plugins/publish/collect_workfile.py
+++ client/ayon_photoshop/plugins/publish/collect_workfile.py
@@ -15,15 +15,14 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
 
     def process(self, instance):
         file_path = instance.context.data["currentFile"]
-        _, ext = os.path.splitext(file_path)
+        ext = os.path.splitext(file_path)[1].lstrip(".")
         staging_dir = os.path.dirname(file_path)
         base_name = os.path.basename(file_path)
 
         # creating representation
-        _, ext = os.path.splitext(file_path)
         instance.data["representations"].append({
-            "name": ext[1:],
-            "ext": ext[1:],
+            "name": ext,
+            "ext": ext,
             "files": base_name,
             "stagingDir": staging_dir,
         })
