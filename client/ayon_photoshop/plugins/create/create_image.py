47bf8e21f7f4bf10af97f5e585e936ca6c36c726
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2026-07-16 14:18:45 +0800

    Update client/ayon_photoshop/plugins/create/create_image.py
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_photoshop/plugins/create/create_image.py
+++ client/ayon_photoshop/plugins/create/create_image.py
@@ -166,7 +166,7 @@ class ImageCreator(Creator):
         for instance in instances:
             members = instance.data.get("members")
             if members:
-                members = [int(member) for member in members]
+                members = {int(member) for member in members}
                 api.stub().delete_group_instance(members)
             self.host.remove_instance(instance)
             self._remove_instance_from_context(instance)
